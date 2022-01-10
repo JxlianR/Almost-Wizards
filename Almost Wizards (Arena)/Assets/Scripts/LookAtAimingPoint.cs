ac3e694a6f619ac1c0fd145df0a174b1a662514b
JxlianR <j.reiter022002@gmail.com>
2022-01-10 19:26:41 +0100

    Uploaded the whole project

---
+++ Almost Wizards (Arena)/Assets/Scripts/LookAtAimingPoint.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtAimingPoint : MonoBehaviour
+{
+    public GameObject target; // GameObject the player will rotate to
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Rotating the player to the position of target GameObject
+        Vector3 positionToRotate = new Vector3(target.transform.position.x, transform.position.y, target.transform.position.z);
+        transform.LookAt(positionToRotate);
+    }
+}
