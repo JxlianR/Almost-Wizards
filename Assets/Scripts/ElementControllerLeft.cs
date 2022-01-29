82e00af996ab59be80f54a0b11a77844591b9591
JxlianR <j.reiter022002@gmail.com>
2022-01-30 00:14:07 +0100

    Fixed bugs

--- Assets/Scripts/ElementControllerLeft.cs
+++ Assets/Scripts/ElementControllerLeft.cs
@@ -39,7 +39,7 @@ public class ElementControllerLeft : MonoBehaviour
     {
         //transform.Translate(Vector3.forward * speed * Time.deltaTime);
         //transform.position = Vector3.MoveTowards(transform.position, new Vector3(PlayerController.aimPositionOne.x, PlayerController.aimPositionOne.y, PlayerController.aimPositionOne.z), speed * Time.deltaTime);
-        transform.Translate(new Vector3(0, 0.3f, 1) * speed * Time.deltaTime);
+        transform.Translate(new Vector3(0, -0.3f, 1) * speed * Time.deltaTime);
     }
 
     private void OnTriggerEnter(Collider other)
